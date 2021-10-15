3ea91acaef10ef97ffb7669ecd2e07551f09ae2f
Nurillh <92520336+Nurillh@users.noreply.github.com>
2021-10-15 23:05:46 +0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -511,18 +511,22 @@ async function sortBuku(order){
   await ambildataListBuku().then(()=>console.log(Buku.getKategori()))
 }
 
-async function mOver(ket) {
-  ket.innerHTML = `
+document.getElementById("lb-title").onmouseover = function() {mOver()};
+document.getElementById("lb-title").onmouseout = function() {mOut()};
+
+async function mOver() {
+  document.getElementById("lb-title").innerHTML = `
                     <p style="margin:0px;">List Buku</p>
                     <h6 style="margin:3px; background: linear-gradient(#3CB371, #9fdfbc); color:#000000; border-radius: 0px 0px 5px 5px;">Silahkan Pilih Tipe Pencarian</h6>
                     `
 }
 
 async function mOut(ket) {
-  ket.innerHTML = `
+  document.getElementById("lb-title").innerHTML = `
   List Buku
   `
 }
+
 
 //panggil fungsi
 
